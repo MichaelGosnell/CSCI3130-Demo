0fa5495751a2ab16ab33caa6cf6b2051fb537110
Michael Gosnell <michaeljgosnell@gmail.com>
2018-01-23 22:01:11 -0400

    added elements and button functionality

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView main;
+    EditText entry;
+    Button celebrate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        main = (TextView) findViewById(R.id.main);
+        celebrate = (Button) findViewById(R.id.celebrate);
+        entry = (EditText) findViewById(R.id.textEntry);
+
+        celebrate.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                main.setText(entry.getText());
+            }
+        });
     }
 }
